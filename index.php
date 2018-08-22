8c2448c99aa67fcc93e66a635612e184dbcb05fc
Yair fernando <sistemasalbardas.2@gmail.com>
2018-08-21 20:17:48 -0500

    commit de actualizacion

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', realpath(dirname(__FILE__)) . DS);
 
-	define('URL', "http://localhost/registry/");
+	define('URL', "http://localhost/proyecto_utp/");
 	define('URL_IMG', URL."VIEWS/resources/IMG/");
 
 	require_once "CONFIG/autoload.php";
